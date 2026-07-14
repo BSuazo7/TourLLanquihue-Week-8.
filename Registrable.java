33f96964d8c27d26198b2222639b21c258061b4d
Benjamin Suazo <be.suazoc@duocuc.cl>
2026-07-13 23:54:22 -0400

    Create Registrable.java

---
+++ Registrable.java
@@ -0,0 +1,10 @@
+package model;
+
+/**
+ * Interfaz que define un contrato común para todas las entidades
+ * que pueden ser registradas en el sistema de Llanquihue Tour.
+ */
+public interface Registrable {
+    // Método abstracto que debe ser implementado por las clases
+    void mostrarResumen();
+}
